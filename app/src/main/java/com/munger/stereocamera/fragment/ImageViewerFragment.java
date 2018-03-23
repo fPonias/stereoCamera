63e16d5a8c4ad5919ff0165cae6640986ee4af87
Cody Munger <cody.munger@hallmarklabs.com>
2018-03-22 22:13:03 -0700

    tweaked gallery viewer

--- app/src/main/java/com/munger/stereocamera/fragment/ImageViewerFragment.java
+++ app/src/main/java/com/munger/stereocamera/fragment/ImageViewerFragment.java
@@ -2,6 +2,9 @@ package com.munger.stereocamera.fragment;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -18,7 +21,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.munger.stereocamera.BaseActivity;
 import com.munger.stereocamera.MainActivity;
@@ -32,9 +37,9 @@ import java.util.ArrayList;
 
 public class ImageViewerFragment extends Fragment
 {
-	ViewPager pager;
+	private ViewPager pager;
 	private ImagePagerAdapter adapter;
-	private ActionBar toolbar;
+	private ImageButton playButton;
 
 	@Nullable
 	@Override
@@ -47,14 +52,177 @@ public class ImageViewerFragment extends Fragment
 		{
 			openTopBar();
 		}});
+		pager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() { public void onPageSelected(int position)
+		{
+			super.onPageSelected(position);
+			pageIndex = position;
+			updateLabel(position);
+		}});
 
-		BaseActivity act = MyApplication.getInstance().getCurrentActivity();
-		toolbar = act.getSupportActionBar();
+		playButton = ret.findViewById(R.id.play_button);
+		playButton.setOnClickListener(new View.OnClickListener() { public void onClick(View v)
+		{
+			togglePlayback();
+		}});
 
 		setHasOptionsMenu(true);
 
 		return ret;
 	}
+
+	private void resetLabel()
+	{
+		if (menuTitle != null)
+			setLabel(menuTitle);
+	}
+
+	private void updateLabel(int index)
+	{
+		if (menuTitle != null)
+		{
+			int sz = adapter.getCount();
+			int idx = index + 1;
+			String newTitle = menuTitle + " (" + idx + "/" + sz + ")";
+
+			setLabel(newTitle);
+		}
+	}
+
+	private void setLabel(String label)
+	{
+		if (adapter != null)
+		{
+			MainActivity activity = (MainActivity) getActivity();
+
+			if (activity != null)
+			{
+				ActionBar bar = activity.getSupportActionBar();
+
+				if (bar != null)
+					bar.setTitle(label);
+			}
+		}
+	}
+
+	private int pageIndex = 0;
+	private final Object lock = new Object();
+	private Thread playbackThread = null;
+	private boolean isPlaying = false;
+	private PlaybackUpdateRunnable playbackUpdateRunnable = new PlaybackUpdateRunnable();
+	private int PLAYBACK_DELAY = 3000;
+	private Handler handler = null;
+
+	private void togglePlayback()
+	{
+		if (isPlaying)
+		{
+			stopPlayback();
+		}
+		else
+		{
+			startPlayback();
+		}
+	}
+
+	private void startPlayback()
+	{
+		synchronized (lock)
+		{
+			if (isPlaying)
+				return;
+
+			if (handler == null)
+				handler = new Handler(Looper.getMainLooper());
+
+			playbackThread = new Thread(togglePlaybackLoop);
+			isPlaying = true;
+			playbackThread.start();
+
+			playButton.setImageDrawable(getResources().getDrawable(R.drawable.pause));
+		}
+	}
+
+	private void stopPlayback()
+	{
+		synchronized (lock)
+		{
+			if (!isPlaying)
+				return;
+
+			isPlaying = false;
+			lock.notify();
+
+			if (playbackThread != null)
+				try {lock.wait(PLAYBACK_DELAY);} catch(InterruptedException e){}
+
+			playButton.setImageDrawable(getResources().getDrawable(R.drawable.play_arrow));
+		}
+	}
+
+	private class PlaybackUpdateRunnable implements Runnable
+	{
+		private int index = 0;
+		private boolean smoothScroll = false;
+
+		public void setIndex(int idx)
+		{
+			index = idx;
+		}
+
+		public void setSmoothScroll(boolean smoothScroll)
+		{
+			this.smoothScroll = smoothScroll;
+		}
+
+		public void run()
+		{
+			try
+			{
+				updateLabel(index);
+			}
+			catch(NullPointerException e){}
+
+			pager.setCurrentItem(index, smoothScroll);
+		}
+
+		public void doScroll(int index, boolean smoothScroll)
+		{
+			playbackUpdateRunnable.setIndex(index);
+			playbackUpdateRunnable.setSmoothScroll(smoothScroll);
+			handler.post(this);
+		}
+	}
+
+	private Runnable togglePlaybackLoop = new Runnable() { public void run()
+	{
+		int sz = adapter.getCount();
+
+		int i = 0;
+		playbackUpdateRunnable.doScroll(0, false);
+		while (isPlaying && i < sz)
+		{
+			playbackUpdateRunnable.doScroll(i, true);
+
+			synchronized (lock)
+			{
+				try { lock.wait(PLAYBACK_DELAY); } catch (InterruptedException e) {break;}
+
+				if (!isPlaying)
+					break;
+			}
+
+			i++;
+		}
+
+		synchronized (lock)
+		{
+			isPlaying = false;
+			playbackThread = null;
+			lock.notify();
+		}
+	}};
+
+	private String menuTitle;
 
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
@@ -64,9 +232,71 @@ public class ImageViewerFragment extends Fragment
 		deleteMenuItem = menu.findItem(R.id.delete);
 		deleteMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() { public boolean onMenuItemClick(MenuItem menuItem)
 		{
+			stopPlayback();
 			deleteCurrent();
 			return true;
 		}});
+
+		try
+		{
+			MainActivity activity = (MainActivity) getActivity();
+
+			if (activity != null)
+			{
+				ActionBar bar = activity.getSupportActionBar();
+
+				if (bar != null)
+				{
+					CharSequence seq = bar.getTitle();
+
+					if (seq != null)
+						menuTitle = seq.toString();
+				}
+			}
+
+			updateLabel(pageIndex);
+		}
+		catch(NullPointerException e){}
+	}
+
+	@Override
+	public void onSaveInstanceState(@NonNull Bundle outState)
+	{
+		super.onSaveInstanceState(outState);
+		outState.putInt("index", pageIndex);
+	}
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		if (savedInstanceState != null)
+		{
+			pageIndex = savedInstanceState.getInt("index", 0);
+		}
+		else
+		{
+			pageIndex = 0;
+		}
+	}
+
+	@Override
+	public void onPause()
+	{
+		super.onPause();
+
+		if (isPlaying)
+		{
+			stopPlayback();
+		}
+	}
+
+	@Override
+	public void onStop()
+	{
+		super.onStop();
+		resetLabel();
 	}
 
 	public MenuItem deleteMenuItem;
@@ -106,7 +336,7 @@ public class ImageViewerFragment extends Fragment
 		File f = new File(selectedPath);
 
 		if (f.exists())
-			f.delete();
+		{}	//f.delete();
 
 		adapter.deleteItem(idx);
 
@@ -138,6 +368,7 @@ public class ImageViewerFragment extends Fragment
 			public void done()
 			{
 				adapter = new ImagePagerAdapter(getFragmentManager(), photoFiles);
+				updateLabel(0);
 				pager.setAdapter(adapter);
 			}
 
@@ -196,7 +427,7 @@ public class ImageViewerFragment extends Fragment
 		public void deleteItem(int position)
 		{
 			int sz = files.length;
-			String[] newFiles = new String[sz];
+			String[] newFiles = new String[sz - 1];
 
 			for (int i = 0; i < position; i++)
 				newFiles[i] = files[i];
@@ -204,6 +435,7 @@ public class ImageViewerFragment extends Fragment
 			for (int i = position + 1; i < sz; i++)
 				newFiles[i - 1] = files[i];
 
+			files = newFiles;
 			notifyDataSetChanged();
 		}
 
@@ -224,19 +456,19 @@ public class ImageViewerFragment extends Fragment
 	{
 		private ImageView thumbnailView;
 		private String path;
-		private View root;
 
 		@Nullable
 		@Override
 		public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
 		{
-			root = inflater.inflate(R.layout.fragment_thumbnail_collection_object, container, false);
-			thumbnailView = root.findViewById(R.id.image);
+			thumbnailView = new ImageView(getContext());
+			ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+			thumbnailView.setLayoutParams(params);
 
 			Bundle args = getArguments();
 			path = args.getString("file");
 
-			return root;
+			return thumbnailView;
 		}
 
 		@Override
